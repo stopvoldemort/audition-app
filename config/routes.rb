f0036fc02633528116e05f03c06ef62dea80171f
stopvoldemort <david.squires1@gmail.com>
2017-10-05 16:28:18 -0400

    styled audition-requests page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: 'sessions#new'
+
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   post '/signout', to: 'sessions#destroy'
